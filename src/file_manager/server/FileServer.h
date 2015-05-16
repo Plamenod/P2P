ac77225dd7b86d5df6358c804593985c90f5e24c
Plamen Dimitrov <plamenod@gmail.com>
2015-05-16 16:27:07 +0300

    Initial file for file server

---
+++ src/file_manager/server/FileServer.h
@@ -0,0 +1,17 @@
+/*
+ * FileServer.h
+ *
+ *  Created on: May 16, 2015
+ *      Author: plamen
+ */
+
+#ifndef SRC_FILE_MANAGER_SERVER_FILESERVER_H_
+#define SRC_FILE_MANAGER_SERVER_FILESERVER_H_
+
+class FileServer {
+public:
+	FileServer();
+	virtual ~FileServer();
+};
+
+#endif /* SRC_FILE_MANAGER_SERVER_FILESERVER_H_ */
