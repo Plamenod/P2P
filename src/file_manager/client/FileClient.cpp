2316b19217a5231d2187ccc229fb0879d56cb2d4
Minh Nguyen <minh0722@yahoo.com>
2015-05-16 17:05:45 +0300

    initial file for file client

---
+++ src/file_manager/client/FileClient.cpp
@@ -0,0 +1,17 @@
+/*
+ * FileClient.cpp
+ *
+ *  Created on: May 16, 2015
+ *      Author: minh
+ */
+
+#include "FileClient.h"
+
+FileClient::FileClient() {
+
+}
+
+FileClient::~FileClient() {
+
+}
+
