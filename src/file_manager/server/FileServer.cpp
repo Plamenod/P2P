6644d724e71e4a3ab40119bc6937dae7fbad3224
Plamen Dimitrov <plamenod@gmail.com>
2015-05-17 20:52:00 +0300

    added initial_append

--- src/file_manager/server/FileServer.cpp
+++ src/file_manager/server/FileServer.cpp
@@ -22,44 +22,64 @@
 
 using namespace std;
 FileServer::FileServer() : buffer(unique_ptr<char[]>( new char[SIZE_BUFFER])) {
+
+    info.size_of_file = 0;
+    fd = fopen("tmp.dat", "ab+");
+    if(fd == NULL)
+    {
+        std::cerr << "can't open the file" << std::endl;
+        exit(1);
+    }
+    bzero(buffer.get(), SIZE_BUFFER);
+    info.id = 100; // fake id, it's only for testing
+}
+
+FileServer::~FileServer() {
+    fclose(fd);
 }
 
 bool FileServer::receive(unsigned short host_port) {
 
-    Socket socket;
     socket.bindTo(host_port);
-
     listen(socket, MAX_LENGTH_OF_QUEUE_PANDING);
-
-
-    int  newsockfd = accept( socket, nullptr,nullptr);
+    int  newsockfd = accept(socket, nullptr, nullptr);
 
     if (newsockfd < 0) {
         std::cerr << "ERROR on accept\n";
         exit(1);  // TODO remove it
     }
 
-    bzero(buffer.get(), SIZE_BUFFER);
+    initial_append(newsockfd);
+
 
     int readed_bytes = -1;
-    uint64_t size_of_file;
-
-    read(newsockfd, &size_of_file, sizeof(uint64_t));
-
-    printf("size_of_file: %ld\n", size_of_file);
-
     while(readed_bytes = read(newsockfd, buffer.get(), SIZE_BUFFER - 1)) {
 
         printf("Here is the message: %s\n", buffer.get());
+
+
         scanf("%s", buffer.get());
         readed_bytes = write(newsockfd, buffer.get(),SIZE_BUFFER);
         printf("bytes %d\n", readed_bytes);
+
+        bzero(buffer.get(), SIZE_BUFFER); // TODO remove to append_to_file, after appended it
      }
 
      if (readed_bytes < 0) {
         std::cerr << "ERROR writing to socket";
      }
-
     return true/*TODO*/;
 }
 
+/*you have to have the id before use it*/
+bool FileServer::initial_append(int newsockfd) {
+    // get size of the file from the client
+    read(newsockfd, &info.size_of_file, sizeof(uint64_t));
+    printf("size_of_file: %ld\n", info.size_of_file);
+
+    return fwrite(&info, sizeof(InfoData), 1, fd);
+}
+
+bool FileServer::append_to_file() {
+    return false;
+}
--- src/file_manager/server/FileServer.h
+++ src/file_manager/server/FileServer.h
@@ -14,15 +14,32 @@
 
 #define SIZE_BUFFER 4000
 #include <memory>
+#include "socket.h"
+#include <cstdint>
+
+struct InfoData{
+    uint64_t size_of_file;
+    uint64_t id;
+};
 
 class FileServer {
 public:
 	FileServer();
+	~FileServer();
 
     virtual bool receive( unsigned short host_port);
+    /*append only size of file and unique id*/
+    virtual bool initial_append(int);
+    /*append current buffer*/
+    virtual bool append_to_file();
 
 private:
     std::unique_ptr<char[]> buffer;
+    Socket socket;
+    FILE* fd;
+    InfoData info;
+
+//    std::vector<uint64_t> all_ids;
 };
 
 #endif /* SRC_FILE_MANAGER_SERVER_FILESERVER_H_ */
