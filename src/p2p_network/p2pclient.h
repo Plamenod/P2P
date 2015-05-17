cf990c7fc575a6751abc247ce325b03a7e15d1eb
stoyanh <stoqnhristov93@gmail.com>
2015-05-17 17:22:52 +0300

    p2pclient asks server for other peers

--- src/p2p_network/p2pclient.h
+++ src/p2p_network/p2pclient.h
@@ -2,9 +2,15 @@
 #define P2PCLIENT_H_INCLUDED
 
 #include "socket.h"
+#include "commondefines.h"
 #include <string>
+#include <vector>
 
-const unsigned short port = 7777;
+const unsigned short port = 2332;
+
+enum command: char {
+    GET_PEERS = '1',
+};
 
 class P2PClient
 {
@@ -12,13 +18,20 @@ public:
     P2PClient(){}
     ~P2PClient();
 
-    void start() const;
+    void setServerIP(const std::string& ip);
+
+    void connectToServer(const std::string& ip);
+    void acquirePeersInfo() const;
 
     P2PClient(const P2PClient&) = delete;
     P2PClient& operator=(const P2PClient&) = delete;
 
 private:
+    size_t send(const void* buf, size_t size, int flags = 0) const;
+
+
     Socket socket;
+    std::string server_ip;
 };
 
 #endif // P2PCLIENT_H_INCLUDED
