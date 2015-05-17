fa551eb3f3fed1ab5f243b91665b85deb7d431ff
Minh Nguyen <minh0722@yahoo.com>
2015-05-17 12:22:14 +0300

    removed pure virtual in file_manager_interface destructor

--- src/common/file_manager_interface.h
+++ src/common/file_manager_interface.h
@@ -7,7 +7,7 @@
 class FileManagerInterface {
 public:
 	virtual bool send(const std::string & host, std::string & file_path, uint64_t from = 0, uint64_t to = -1) = 0;
-	virtual ~FileManagerInterface() = 0;
+	virtual ~FileManagerInterface(){};
 };
 
 #endif // _FILE_MANAGER_INTERFACE_
