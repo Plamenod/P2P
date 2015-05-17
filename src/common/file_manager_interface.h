18b2b5d850a7fb2bad983b25d8f619003c00c027
Minh Nguyen <minh0722@yahoo.com>
2015-05-17 12:32:47 +0300

    added argument host_port in connect()

--- src/common/file_manager_interface.h
+++ src/common/file_manager_interface.h
@@ -6,7 +6,12 @@
 
 class FileManagerInterface {
 public:
-	virtual bool send(const std::string & host, std::string & file_path, uint64_t from = 0, uint64_t to = -1) = 0;
+	virtual bool send(
+                const std::string & host,
+                const unsigned short host_port,
+                std::string & file_path,
+                uint64_t from = 0,
+                uint64_t to = -1) = 0;
 	virtual ~FileManagerInterface(){};
 };
 
