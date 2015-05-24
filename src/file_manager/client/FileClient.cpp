c40f05f26ee0c670979df0f9d2e7208acda4948a
Minh Nguyen <minh0722@yahoo.com>
2015-05-25 01:29:53 +0300

    fixes in ::send() and getFile()

--- src/file_manager/client/FileClient.cpp
+++ src/file_manager/client/FileClient.cpp
@@ -17,143 +17,156 @@ FileClient::~FileClient() {
 }
 
 uint64_t FileClient::send(
-    const std::string & host,
-    std::string & file_path,
-    uint64_t from,
-    uint64_t to)
+	const std::string & host,
+	std::string & file_path,
+	uint64_t from,
+	uint64_t to)
 {
-    uint64_t data_length = to - from;
-
-    if(data_length <= 0) {
-        std::cerr << "Length is <= 0" << std::endl;
-        return 0;
-    }
-
-    std::string ip = getHost(host);
-    unsigned short host_port = getPort(host);
-
-    Socket host_socket;
-    host_socket.connectTo(ip, host_port);
-
-    if(!sendNumber(host_socket, data_length)) {
-        std::cerr << "Failed to send file of length - " << data_length << std::endl;
-    }
-
-    FILE* file_to_send;
-    file_to_send = fopen(file_path.c_str(), "r");
-
-    if(!file_to_send) {
-        std::cerr << "Failed to open file to send" << std::endl;
-        fclose(file_to_send);
-        return 0;
-    }
-
-    if(fseek(file_to_send, from, SEEK_SET)) {
-        std::cerr << "Failed to seek to position " << from << std::endl;
-        fclose(file_to_send);
-        return 0;
-    }
+	uint64_t data_length = to - from;
 
 
-    std::unique_ptr<char[]> buffer(new char[MAX_SIZE + 1]);
-//    std::unique_ptr<char> buffer(new char);
+	if (data_length <= 0) {
+		std::cerr << "Length is <= 0" << std::endl;
+		return 0;
+	}
 
-    while(data_length) {
-        fread(buffer.get(), sizeof(char), MAX_SIZE, file_to_send);
-        data_length -= MAX_SIZE;
+	std::string ip = getHost(host);
+	unsigned short host_port = getPort(host);
 
-        if(ferror(file_to_send)) {
-            std::cerr << "Failed to read file content" << std::endl;
-            fclose(file_to_send);
-            return 0;
-        }
+	Socket host_socket;
+	host_socket.connectTo(ip, host_port);
 
-//        std::cout << buffer.get();
+	uint64_t send_file_event = 0;
+	if (!sendNumber(host_socket, send_file_event)) {
+		std::cerr << "Failed to send event" << std::endl;
+		return 0;
+	}
 
-        int bytes_sent = ::send(host_socket.getFd(), buffer.get(), strlen(buffer.get()), 0);
+	if (!sendNumber(host_socket, data_length)) {
+		std::cerr << "Failed to send file of length - " << data_length << std::endl;
+		return 0;
+	}
 
-        if(bytes_sent == -1) {
-            std::cerr << "Failed to write to socket" << std::endl;
-            fclose(file_to_send);
-            return 0;
-        }
+	FILE* file_to_send;
+	file_to_send = fopen(file_path.c_str(), "r");
 
-        if(feof(file_to_send)) {
-            break;
-        }
-    }
+	if (!file_to_send) {
+		std::cerr << "Failed to open file to send" << std::endl;
+		fclose(file_to_send);
+		return 0;
+	}
 
-    uint64_t fileID = getFileID(host_socket);
-//    std::cout << "FileID: " << fileID << std::endl;
+	if (fseek(file_to_send, from, SEEK_SET)) {
+		std::cerr << "Failed to seek to position " << from << std::endl;
+		fclose(file_to_send);
+		return 0;
+	}
 
-    fclose(file_to_send);
-    return fileID;
+
+	std::unique_ptr<char[]> buffer(new char[MAX_SIZE + 1]);
+
+	while (data_length) {
+		int byte_read = fread(buffer.get(), sizeof(char), MAX_SIZE, file_to_send);
+		data_length -= byte_read;
+
+		if (ferror(file_to_send)) {
+			std::cerr << "Failed to read file content" << std::endl;
+			fclose(file_to_send);
+			return 0;
+		}
+
+		int bytes_sent = ::send(host_socket.getFd(), buffer.get(), byte_read, 0);
+
+		if (bytes_sent == -1) {
+			std::cerr << "Failed to write to socket" << std::endl;
+			fclose(file_to_send);
+			return 0;
+		}
+	}
+
+	uint64_t fileID = getFileID(host_socket);
+	//    std::cout << "FileID: " << fileID << std::endl;
+
+	fclose(file_to_send);
+	return fileID;
 }
 
 std::unique_ptr<char[]> FileClient::getFile(const std::string& host, uint64_t id) {
-    std::string ip = getHost(host);
-    unsigned short port = getPort(host);
+	std::string ip = getHost(host);
+	unsigned short port = getPort(host);
 
-    Socket host_socket;
-    host_socket.connectTo(ip, port);
+	Socket host_socket;
+	host_socket.connectTo(ip, port);
 
-    if(!sendNumber(host_socket, id)) {
-        std::cerr << "Failed to send file ID" << std::endl;
-        return nullptr;
-    }
+	uint64_t request_file_event = 1;
+	if (!sendNumber(host_socket, request_file_event)) {
+		std::cerr << "Failed to send event" << std::endl;
+		return nullptr;
+	}
 
-    uint64_t file_size;
-    if(::recv(host_socket.getFd(), reinterpret_cast<char*>(&file_size), sizeof(uint64_t), 0) == -1) {
-        std::cerr << "Failed to receive file size" << std::endl;
-        return nullptr;
-    }
+	if (!sendNumber(host_socket, id)) {
+		std::cerr << "Failed to send file ID" << std::endl;
+		return nullptr;
+	}
 
-    std::cout << "File size: " << file_size << std::endl;
+	uint64_t file_size;
+	if (::recv(host_socket.getFd(), reinterpret_cast<char*>(&file_size), sizeof(uint64_t), 0) != sizeof(uint64_t)) {
+		std::cerr << "Failed to receive file size" << std::endl;
+		return nullptr;
+	}
 
-    std::unique_ptr<char[]> file_content(new char[file_size+1]);
+	std::cout << "File size: " << file_size << std::endl;
 
-    while(file_size) {
-        int byte_read = ::recv(
-                            host_socket.getFd(),
-                            reinterpret_cast<char*>(file_content.get()),
-                            file_size,
-                            0);
+	std::unique_ptr<char[]> file_content(new char[file_size]);
 
-        printf("byte read: %d \n Message: %s\n", byte_read, file_content.get());
+	//TODO: remove
+	uint64_t x;
+	::recv(host_socket, reinterpret_cast<char*>(&x), sizeof(x), 0);
+	std::cout << x << std::endl;
+	::recv(host_socket, reinterpret_cast<char*>(&x), sizeof(x), 0);
+	std::cout << x << std::endl;
+	file_size -= 8 * 2;
 
-        memset(file_content.get(), 0, BYTE_TO_RECEIVE);
-        file_size -= byte_read;
-    }
+	int offset = 0;
+	while (file_size > 0) {
+		int byte_read = ::recv(
+			host_socket.getFd(),
+			reinterpret_cast<char*>(file_content.get() + offset),
+			file_size,
+			0);
 
-//    std::cout << file_content.get() << std::endl;
+		//printf("byte read: %d \n Message: %s\n", byte_read, file_content.get());
 
-    return std::move(file_content);
+		file_size -= byte_read;
+		offset += byte_read;
+	}
+	//printf("byte read: %d \n Message: %s\n", offset, file_content.get());
+
+	return std::move(file_content);
 }
 
 bool FileClient::sendNumber(const Socket& host_socket, uint64_t number) {
 
-    int byte_sent = ::send(
-                        host_socket.getFd(),
-                        reinterpret_cast<const char *>(&number),
-                        sizeof(uint64_t),
-                        0);
+	int byte_sent = ::send(
+		host_socket.getFd(),
+		reinterpret_cast<const char *>(&number),
+		sizeof(number),
+		0);
 
-    if(byte_sent == -1) {
-        return false;
-    }
-    return true;
+	if (byte_sent != sizeof(number)) {
+		return false;
+	}
+	return true;
 }
 
 uint64_t FileClient::getFileID(const Socket& host_socket) {
-    uint64_t file_id;
-    int file_read = ::recv(host_socket.getFd(), reinterpret_cast<char *>(&file_id), sizeof(uint64_t), 0);
+	uint64_t file_id;
+	int file_read = ::recv(host_socket.getFd(), reinterpret_cast<char *>(&file_id), sizeof(uint64_t), 0);
 
-    if(file_read == -1) {
-        std::cerr << "Failed to read from socket" << std::endl;
-        return 0;
-    }
+	if (file_read != sizeof(uint64_t)) {
+		std::cerr << "Failed to read from socket" << std::endl;
+		return 0;
+	}
 
-    return file_id;
+	return file_id;
 }
-
-
