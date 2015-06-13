b6fafddcdcb1bf23566491f761085742f12f97d4
stoyan <stoqnhristov93@gmail.com>
2015-06-13 20:24:26 +0300

    Add back main

---
+++ src/main.cpp
@@ -0,0 +1,77 @@
+#include <memory>
+#include <iostream>
+#include <sstream>
+
+#include "file_manager/FileManager.h"
+#include "p2p_network/p2p_manager/p2pnode.h"
+#include "app/app.h"
+
+using namespace std;
+
+int main(int argc, char * argv[]) {
+
+    if (argc != 6) {
+        std::cerr << "App port, File mgr port, Server port, Main server ip, Main server port\n";
+        return 1;
+    }
+
+    App::Settings settings;
+    settings.app_port = 3001;
+    settings.file_mgr_port = 3002;
+    settings.server_port = 3003;
+
+    settings.main_server = "127.0.0.1";
+    settings.ms_port = 5005;
+
+    std::stringstream strm;
+    for (int c = 1; c < argc; ++c) {
+        strm << argv[c] << ' ';
+    }
+
+    strm >> settings.app_port >> settings.file_mgr_port >> settings.server_port
+         >> settings.main_server >> settings.ms_port;
+
+    App app(
+        settings,
+        std::unique_ptr<FileManagerInterface>(new FileManager(settings.file_mgr_port)),
+        std::unique_ptr<P2PNetworkInterface>(new P2PNode(settings.ms_port, settings.server_port, settings.file_mgr_port)));
+
+    app.run();
+
+    while (1) {
+        std::string fileName, exportName;
+
+        cout << "Enter file path to import:\n";
+        std::getline(cin, fileName);
+        cout << "Enter file path to export:\n";
+        std::getline(cin, exportName);
+
+        if (!app.importFile(fileName)) {
+            std::cerr << "Failed to add file to storage";
+            return 0;
+        } else {
+            std::cout << "Imported!\n";
+        }
+
+        auto r = app.isFileInStorage(fileName);
+        if (r != App::FileAvailability::High) {
+            std::cerr << "File should be in in High availability";
+            return 0;
+        } else {
+            std::cout << "Available!\n";
+        }
+
+        if (!app.exportFile(fileName, exportName)) {
+            std::cerr << "Failed to export file from storage";
+            return 0;
+        } else {
+            std::cout << "Exported!\n";
+        }
+        
+    }
+
+
+    std::cout << "\n\nAll good exiting";
+    std::cin.get();
+    return 0;
+}
